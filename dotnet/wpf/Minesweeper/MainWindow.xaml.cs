dd556fdd141e1dddf7bbeb480abb45806ba2fd8d
Volodymyr Holovko <vgolovko-x@tunein.com>
2019-06-24 18:25:56 +0300

    Initial commit.

---
+++ dotnet/wpf/Minesweeper/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using Minesweeper.SWidgets;
+using Sodium.Frp;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach( var index in Enumerable.Range( 0, 80 ) )
+            {
+                Transaction.RunVoid( () =>
+                 {
+                     CellLoop<SolidColorBrush> background = new CellLoop<SolidColorBrush>();
+                     CellLoop<bool> enabled = new CellLoop<bool>();
+
+                     var sButton = new SButton( background, enabled ) { Width = 24, Height = 24 };
+
+                     Stream<SolidColorBrush> sUpdateBackground = sButton.SClicked.Map( u => SystemColors.ControlDarkBrush );
+                     background.Loop( sUpdateBackground.Hold( SystemColors.ControlLightBrush ) );
+
+                     Stream<bool> sUpdateEnabled = sButton.SClicked.Map( u => false );
+                     enabled.Loop( sUpdateEnabled.Hold( true ) );
+
+                     Container.Children.Add( sButton );
+                 } );
+            }
+        }
+    }
+}
---
+++ dotnet/wpf/Minesweeper/SWidgets/SButton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Sodium.Frp;
+using Sodium.Functional;
+
+namespace Minesweeper.SWidgets
+{
+    public class SButton : Button, IDisposable
+    {
+        private readonly IReadOnlyList<IListener> listeners;
+
+        public SButton()
+            : this( Cell.Constant( SystemColors.ControlLightBrush ), Cell.Constant(true) )
+        {
+        }
+
+        public SButton( Cell<SolidColorBrush> background, Cell<bool> enabled )
+        {
+            StreamSink<Unit> sClickedSink = Stream.CreateSink<Unit>();
+            SClicked = sClickedSink;
+            Click += ( sender, args ) => sClickedSink.Send( Unit.Value );
+
+            // Set the initial value at the end of the transaction so it works with CellLoops.
+            Transaction.Post( () => Background = background.Sample() );
+            Transaction.Post( () => IsEnabled = enabled.Sample());
+
+            // ReSharper disable once UseObjectOrCollectionInitializer
+            List<IListener> listeners = new List<IListener>();
+            listeners.Add( background.Updates().Listen( e => Dispatcher.InvokeIfNecessary( () => Background = e ) ) );
+            listeners.Add( enabled.Updates().Listen( e => Dispatcher.InvokeIfNecessary( () => IsEnabled = e ) ) );
+            this.listeners = listeners;
+        }
+
+        public Stream<Unit> SClicked { get; }
+
+        public void Dispose()
+        {
+            foreach( IListener l in this.listeners )
+            {
+                l.Unlisten();
+            }
+        }
+    }
+}
