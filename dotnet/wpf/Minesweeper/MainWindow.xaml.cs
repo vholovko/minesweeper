27bc3efbb7039c9735797dcfe4d82cfd83a0d705
Volodymyr Holovko <vgolovko-x@tunein.com>
2019-06-24 19:29:28 +0300

    Added level sizes.

--- dotnet/wpf/Minesweeper/MainWindow.xaml.cs
+++ dotnet/wpf/Minesweeper/MainWindow.xaml.cs
@@ -15,23 +15,45 @@ namespace Minesweeper
         {
             InitializeComponent();
 
-            foreach( var index in Enumerable.Range( 0, 80 ) )
+            const int buttonSize = 24;
+
+            // Introduction
+            //const int columns = 8;
+            //const int rows = 8;
+            // const int mines = 10;
+
+            // Intermediate
+            // const int columns = 16;
+            // const int rows = 16;
+            // const int mines = 40;
+
+            // Expert
+            const int columns = 30;
+            const int rows = 16;
+            // const int mines = 99;
+
+            const int total = columns * rows;
+
+            Container.MaxWidth = buttonSize * columns;
+            Container.MaxHeight = buttonSize * rows;
+
+            foreach( var index in Enumerable.Range( 0, total ) )
             {
                 Transaction.RunVoid( () =>
-                 {
-                     CellLoop<SolidColorBrush> background = new CellLoop<SolidColorBrush>();
-                     CellLoop<bool> enabled = new CellLoop<bool>();
+                {
+                    CellLoop<SolidColorBrush> background = new CellLoop<SolidColorBrush>();
+                    CellLoop<bool> enabled = new CellLoop<bool>();
 
-                     var sButton = new SButton( background, enabled ) { Width = 24, Height = 24 };
+                    var sButton = new SButton( background, enabled ) { Width = buttonSize, Height = buttonSize };
 
-                     Stream<SolidColorBrush> sUpdateBackground = sButton.SClicked.Map( u => SystemColors.ControlDarkBrush );
-                     background.Loop( sUpdateBackground.Hold( SystemColors.ControlLightBrush ) );
+                    Stream<SolidColorBrush> sUpdateBackground = sButton.SClicked.Map( u => SystemColors.ControlDarkBrush );
+                    background.Loop( sUpdateBackground.Hold( SystemColors.ControlLightBrush ) );
 
-                     Stream<bool> sUpdateEnabled = sButton.SClicked.Map( u => false );
-                     enabled.Loop( sUpdateEnabled.Hold( true ) );
+                    Stream<bool> sUpdateEnabled = sButton.SClicked.Map( u => false );
+                    enabled.Loop( sUpdateEnabled.Hold( true ) );
 
-                     Container.Children.Add( sButton );
-                 } );
+                    Container.Children.Add( sButton );
+                } );
             }
         }
     }
